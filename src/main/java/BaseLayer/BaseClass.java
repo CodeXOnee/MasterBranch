6d2436425fcbd28b4440c31447d6ffefec28ec34
Yash Purohit <ypurohit988@gmail.com>
2025-08-16 15:19:21 +0530

    Advance Framework

---
+++ src/main/java/BaseLayer/BaseClass.java
@@ -0,0 +1,14 @@
+package BaseLayer;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+	public static WebDriver driver;
+	public static void initialize() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();                                                                           
+		
+	}
+	
+}
