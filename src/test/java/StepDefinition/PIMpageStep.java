dcd01c15387ce59b75d8996b27abd525ea6fa708
Ankit <ankitwankhede999@gmail.com>
2025-08-16 19:35:42 +0530

    write code for PIM page

---
+++ src/test/java/StepDefinition/PIMpageStep.java
@@ -0,0 +1,49 @@
+package StepDefinition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class PIMpageStep {
+	
+	@Given("User is on Home Page")
+	public void user_is_on_home_page() {
+	   
+	}
+
+	@Given("User Enters Valid Credentials")
+	public void user_enters_valid_credentials() {
+	    
+	}
+
+	@When("User Clicks on Login button")
+	public void user_clicks_on_login_button() {
+	   
+	}
+
+	@Then("User should navigate to Dashboard")
+	public void user_should_navigate_to_dashboard() {
+	    
+	}
+
+	@Given("user is on PIM page and valid the PIM page")
+	public void user_is_on_pim_page_and_valid_the_pim_page() {
+	   
+	}
+
+	@When("user click on add employee")
+	public void user_click_on_add_employee() {
+	   
+	}
+
+	@Then("user add employee {string} and {string} and {string}")
+	public void user_add_employee_and_and(String string, String string2, String string3) {
+	    
+	}
+
+	@Then("user click on save button")
+	public void user_click_on_save_button() {
+	    
+	}
+
+}
